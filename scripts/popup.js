23cc0844d322d68939214bcfa95ae7687d513b34
Gaurav Kumar <developergkindia@gmail.com>
2020-07-26 00:16:18 +0530

    UI Updated

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,9 @@
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     console.log(tabs[0].url);
     var currentDomain = tabs[0].url;
-    document.getElementById("title").innerHTML = currentDomain;
+    $("#domain").val(currentDomain);
 });
 
 $("#checkPage").click(function() {
-    alert("Handler for .click() called.");
-  });
+  alert("Handler for .click() called.");
+});
