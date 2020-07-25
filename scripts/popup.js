1d266105df7410b02810c70b69574ee4af9d2fbc
shachindra <shachindra.spidey@gmail.com>
2020-07-26 01:16:26 +0530

    Get Domain from URL

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,21 @@
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     console.log(tabs[0].url);
-    var currentDomain = tabs[0].url;
+	var currentURL = tabs[0].url;
+	var tabURL = new URL(currentURL);
+	var currentDomain = tabURL.hostname;
+
+	// GET API call - https://api.jquery.com/jQuery.get/
+
     $("#domain").val(currentDomain);
 });
 
-$("#checkPage").click(function() {
-  alert("Handler for .click() called.");
+$("#danger").click(function() {
+	var currentDomain = $("#domain").val();
+	console.table(currentDomain);
+
+	// POST API call
+
+	// $.post( "test.php", {domain: "spam", content: "spam"}).done(function(data) {
+    // 	console.table("Data Loaded: " + data);
+  	// });
 });
