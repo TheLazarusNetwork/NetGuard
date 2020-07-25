a4299485f224cf086f0684c583c6c4636b3f2fbe
shachindra <shachindra.spidey@gmail.com>
2020-07-25 21:08:25 +0530

    Active Tab URL

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
-var currentURL = "mydomain.com";
-
-function getDomain() {
-    document.getElementById("title").innerHTML = currentURL;
-}
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    var currentDomain = tabs[0].url;
+    document.getElementById("title").innerHTML = currentDomain;
+});
