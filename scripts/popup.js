b7e3b4134e8933495cee5ad4bd5b8be59e09d976
Gaurav Kumar <developergkindia@gmail.com>
2020-07-26 09:43:27 +0530

    Functionality Updated

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,124 @@
+function getDomainName(tabURL){
+	var tabURL = new URL(tabURL);
+	return tabURL.hostname;
+}
+
+function processDomainStatus(response){
+  safe = response["safe"];
+  spam = response["spam"];
+  adv = response["adv"];
+  spyware = response["spyware"];
+  malware = response["malware"];
+
+  notSafe = spam + adv + spyware + malware;
+
+  if(safe > notSafe){
+    $('#net-guard-title').text('You Are Safe');
+    $('#safe-count').text(safe);
+    $('.bg-orb').removeClass('not-safe');
+    $('#safety-icon').removeClass('icon-exclamation').addClass('icon-check-mark');
+  } else {
+    $('#net-guard-title').text('Not Safe');
+    $('#not-safe-count').text(notSafe);
+    $('.bg-orb').addClass('not-safe');
+    $('#safety-icon').removeClass('icon-check-mark').addClass('icon-exclamation');
+  }
+}
+
+function voteForThisDomain(type){
+  $.ajax({
+    type: "POST",
+    url: "https://netguardapi.herokuapp.com/api/createNetGuard/",
+    data: {
+      'domain': domainName,
+      'content': type
+    }
+  }).done(function(response){
+    processDomainStatus(response);
+  });
+}
+
+function loadApp(domainName){
+  $.ajax({
+    type: "GET",
+    url: "https://netguardapi.herokuapp.com/api/getNetGuard/",
+    data: {
+      'domain': domainName
+    }
+  }).done(function(response){
+
+    $('.loader').animate({
+      opacity: 0
+    }, 500, function(){
+      $("#domain").val(currentDomain);
+      processDomainStatus(response);
+    });
+  }).fail(function(){
+    $('.loader').animate({
+      opacity: 0
+    }, 500, function(){
+      $("#domain").val('Error Occurred. Please Reload!');
+    });
+  });
+}
+
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
-	var currentURL = tabs[0].url;
-	var tabURL = new URL(currentURL);
-	var currentDomain = tabURL.hostname;
+  // Get Domain
+  currentDomain = getDomainName(tabs[0].url);
 
-	// GET API call - https://api.jquery.com/jQuery.get/
+  // Preview Current Domain
+  if('newtab' == currentDomain){
+    $("#domain").val('Open Website First');
+    return;
+  }
 
-    $("#domain").val(currentDomain);
+  setTimeout(() => {
+    loadApp(currentDomain);  
+  }, 500);
 });
 
-$("#danger").click(function() {
-	var currentDomain = $("#domain").val();
-	console.table(currentDomain);
+$("#danger").click(function(){
+	$('#security-status, #user-interaction').animate({
+    height: 0
+  }, 300);
 
-	// POST API call
+  $('#error-selection').animate({
+    height: 260
+  }, 300, function(){
 
-	// $.post( "test.php", {domain: "spam", content: "spam"}).done(function(data) {
-    // 	console.table("Data Loaded: " + data);
-  	// });
+  });
+});
+
+$('#back-error-section').click(function(){
+  $('#security-status').animate({
+    height: 290
+  }, 300);
+  $('#user-interaction').animate({
+    height: 120
+  }, 300);
+
+
+  $('#error-selection').animate({
+    height: 0
+  }, 300);
+});
+
+$('#cta-safe').click(function(){
+  voteForThisDomain('safe');
+});
+
+$('#cta-spam').click(function(){
+  voteForThisDomain('spam');
+});
+
+$('#cta-adv').click(function(){
+  voteForThisDomain('adv');
+});
+
+$('#cta-spyware').click(function(){
+  voteForThisDomain('spyware');
+});
+
+$('#cta-malware').click(function(){
+  voteForThisDomain('malware');
 });
